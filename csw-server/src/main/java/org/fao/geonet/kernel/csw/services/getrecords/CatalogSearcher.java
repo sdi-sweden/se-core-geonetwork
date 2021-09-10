0cc7df1fd51f007731e5cd15adaafc5547721b34
Jose García <josegar74@gmail.com>
2021-09-10 11:39:57 +0200

    Fix for CSW GetRecords returning duplicated results - uncomment code

--- csw-server/src/main/java/org/fao/geonet/kernel/csw/services/getrecords/CatalogSearcher.java
+++ csw-server/src/main/java/org/fao/geonet/kernel/csw/services/getrecords/CatalogSearcher.java
@@ -591,14 +591,14 @@ public class CatalogSearcher implements MetadataRecordSelector {
             Log.debug(Geonet.CSW_SEARCH, "Records matched : " + numHits);
 
         // NEW CODE: use numHits (total number of results)
-        /*searchResults = LuceneSearcher.doSearchAndMakeSummary(numHits, startPosition - 1,
+        searchResults = LuceneSearcher.doSearchAndMakeSummary(numHits, startPosition - 1,
             maxRecords, _lang.presentationLanguage,
             luceneConfig.getSummaryTypes().get(resultType.toString()), luceneConfig,
             reader, _query, wrapSpatialFilter(),
             _sort, taxonomyReader, buildSummary
         );
         hits = searchResults.one();
-        summary = searchResults.two();*/
+        summary = searchResults.two();
         // END NEW CODE: use numHits (total number of results)
 
         // --- retrieve results
